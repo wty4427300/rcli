ca3e724ff558ca6e029e793811831b88046b2e6c
wty4427300 <wty4427300@gmail.com>
2024-05-13 16:53:36 +0800

    base64

--- src/process/b64.rs
+++ src/process/b64.rs
@@ -1,40 +1,49 @@
-use base64::prelude::*;
 use crate::Base64Format;
-use std::fs::File;
-use std::io::Read;
+use anyhow::Result;
+use base64::{
+    engine::general_purpose::{STANDARD, URL_SAFE_NO_PAD},
+    Engine as _,
+};
+use std::{fs::File, io::Read};
 
-pub fn process_encode(input: &str, format: Base64Format) -> anyhow::Result<()> {
-    let mut reader: Box<dyn Read> = if input == "-" {
+pub fn process_encode(input: &str, format: Base64Format) -> Result<()> {
+    let mut reader = get_reader(input)?;
+    let mut buf = Vec::new();
+    reader.read_to_end(&mut buf)?;
+    let encoded = match format {
+        Base64Format::Standard => STANDARD.encode(&buf),
+        Base64Format::UrlSafe => URL_SAFE_NO_PAD.encode(&buf),
+    };
+    println!("{}", encoded);
+    Ok(())
+}
+
+pub fn process_decode(input: &str, format: Base64Format) -> Result<()> {
+    let mut reader = get_reader(input)?;
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf)?;
+    // avoid accidental newlines
+    let buf = buf.trim();
+
+    let decoded = match format {
+        Base64Format::Standard => STANDARD.decode(buf)?,
+        Base64Format::UrlSafe => URL_SAFE_NO_PAD.decode(buf)?,
+    };
+    // TODO: decoded data might not be string (but for this example, we assume it is)
+    let decoded = String::from_utf8(decoded)?;
+    println!("{}", decoded);
+    Ok(())
+}
+
+fn get_reader(input: &str) -> Result<Box<dyn Read>> {
+    let reader: Box<dyn Read> = if input == "-" {
         Box::new(std::io::stdin())
     } else {
         Box::new(File::open(input)?)
     };
-    let mut buf = Vec::new();
-    reader.read_to_end(&mut buf)?;
-    let encode = match format {
-        Base64Format::Standard => BASE64_STANDARD.encode(&buf),
-        Base64Format::UrlSafe => BASE64_URL_SAFE.encode(&buf),
-    };
-    println!("{}", encode);
-    Ok(())
+    Ok(reader)
 }
 
-pub fn process_decode(output: &str, format: Base64Format) -> anyhow::Result<()> {
-    let mut reader: Box<dyn Read> = if output == "-" {
-        Box::new(std::io::stdin())
-    } else {
-        Box::new(File::open(output)?)
-    };
-    let mut buf = Vec::new();
-    reader.read_to_end(&mut buf)?;
-
-    let decode = match format {
-        Base64Format::Standard => BASE64_STANDARD.decode(&buf)?,
-        Base64Format::UrlSafe => BASE64_URL_SAFE.decode(&buf)?,
-    };
-    println!("{}", String::from_utf8(decode)?);
-    Ok(())
-}
 
 #[cfg(test)]
 mod tests {
@@ -42,7 +51,6 @@ mod tests {
 
     #[test]
     fn test() {
-        let str = "-";
-        let _ = process_encode(str, Base64Format::Standard);
+        let _ = process_encode("Cargo.toml", Base64Format::Standard);
     }
 }
