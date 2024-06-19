5ebac6c01dcc8d9925f8fd2cb5861c8cfbd8cfb2
wty4427300 <wty4427300@gmail.com>
2024-06-19 16:22:53 +0800

    http

--- src/cli/http.rs
+++ src/cli/http.rs
@@ -13,7 +13,7 @@ pub enum HttpSubCommand {
 #[derive(Parser, Debug)]
 pub struct HttpServeOpts {
     #[arg(short, long, value_parser = verify_path, default_value = ".")]
-    pub path: PathBuf,
+    pub dir: PathBuf,
     #[arg(short, long, default_value = "8080")]
     pub port: u16,
 }
