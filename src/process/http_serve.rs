6ed26cd8aa393fbf44acbf23b33cdd8201d034fe
v-wangtianyi <v-wangtianyi@360.cn>
2024-08-02 17:38:35 +0800

    删除多余的代码

--- src/process/http_serve.rs
+++ src/process/http_serve.rs
@@ -3,10 +3,10 @@ use std::net::SocketAddr;
 use std::path::PathBuf;
 use std::sync::Arc;
 use std::task::{Context, Poll};
-use tracing::{info, warn};
+use tracing::info;
 use anyhow::Result;
 use axum::body::Body;
-use axum::extract::{Path, State};
+use axum::extract::State;
 use axum::http::StatusCode;
 use axum::response::{Html, IntoResponse, Response};
 use axum::Router;
@@ -43,24 +43,6 @@ pub async fn process_http_server(path: PathBuf, port: u16) -> Result<()> {
     let listener = tokio::net::TcpListener::bind(addr).await?;
     axum::serve(listener, router).await?;
     Ok(())
-}
-
-async fn file_handler(State(_state): State<Arc<HttpServeState>>, Path(_path): Path<String>) -> (StatusCode, String) {
-    let p = std::path::Path::new(&_state.path).join(_path);
-    if !p.exists() {
-        return (StatusCode::NOT_FOUND, format!("file {} not Found", p.display()));
-    } else {
-        match tokio::fs::read_to_string(p).await {
-            Ok(content) => {
-                info!("read {} bytes",content.len());
-                (StatusCode::OK, content)
-            }
-            Err(e) => {
-                warn!("read file error: {:?}", e);
-                (StatusCode::INTERNAL_SERVER_ERROR, e.to_string())
-            }
-        }
-    }
 }
 
 async fn list_dir(
